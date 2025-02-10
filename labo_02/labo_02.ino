c096771d457dd557240f464db53eadb81d1020d1
Nella00 <allendje@yahoo.com>
2025-02-10 17:27:16 -0500

    test

--- labo_02/labo_02.ino
+++ labo_02/labo_02.ino
@@ -1,16 +1,56 @@
-unsigned long currentTime = 0;
+const int ledPins[] = {8, 9, 10, 11};  // Tableau des numéros de broches
+int potentiometerPin = A1;           
+int potentiometerValue = 0;          
+int ledIndex = 0;                   // Index du DEL allume
+const int boutonPin = 2;
 
 void setup() {
   Serial.begin(9600);
-
-  // Ton code ici
-
-
-
-  Serial.println("Setup completed");
+  for (int i = 0; i < 4; i++) {
+    // Initialisation des DEL en sortie
+    pinMode(ledPins[i], OUTPUT); 
+  }
+  pinMode(boutonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  currentTime = millis();
+  potentiometerValue = analogRead(potentiometerPin);
+  int potentio_convertie = map(potentiometerValue, 0, 1023, 0, 20);
+
+  if (potentio_convertie < 5) {
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], LOW);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+  } else if (potentio_convertie < 10) {
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+  } else if (potentio_convertie < 15) {
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[3], LOW);
+  } else {
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[3], HIGH);
+  }
+
+  
+  
+}
+
+void button(){
+  int potentio_convertie = map(potentiometerValue, 0, 1023, 0, 100);
+  int valueBouton = digitalRead(boutonPin);
+  Serial.println(valueBouton);
+
+  if (valueBouton == 0){
+    for (int )
+    Serial.println(potentio_convertie);
+  }
 
 }
